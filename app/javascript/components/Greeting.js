27afbe2e1b004f257473f1d669265359802c9e73
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-07-04 19:57:38 -0300

    Add components

---
+++ app/javascript/components/Greeting.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Greeting extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!!!</h1>
+      </div>
+    );
+  }
+}
+
+export default Greeting;
